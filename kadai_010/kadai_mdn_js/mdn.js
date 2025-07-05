a12339ab17c3b0d8e15ee85c929920e4f0598508
itou-kouki-gif <itou-kouki@tokyo-gas.co.jp>
2025-07-05 12:26:48 +0900

    Add files via upload

---
+++ kadai_010/kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+
+const today = new Date();
+
+const year = today.getFullYear();
+const month = today.getMonth() + 1; // 月は0から始まるので+1
+const day = today.getDate();
+
+const formatted = `${year}年${month}月${day}日`;
+console.log(formatted);
